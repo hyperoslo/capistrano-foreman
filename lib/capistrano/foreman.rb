6e169433bc8bdbaec5966357a1ef059eab3b296f
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-04-24 14:13:17 -0400

    Add configurations for upstart folder & job prefix

--- lib/capistrano/foreman.rb
+++ lib/capistrano/foreman.rb
@@ -1,27 +1,33 @@
 Capistrano::Configuration.instance(:must_exist).load do |configuration|
 
   _cset(:foreman_sudo, 'sudo')
+  _cset(:foreman_upstart_path, '/etc/init/sites')
+  _cset(:foreman_upstart_prefix, '')
 
   namespace :foreman do
     desc "Export the Procfile to Ubuntu's upstart scripts"
     task :export, roles: :app do
-      run "cd #{current_path} && #{foreman_sudo} bundle exec foreman export upstart /etc/init -a foreman-#{application} -u #{user} -l #{shared_path}/log #{concurrency}"
+      run "cd #{current_path} && #{foreman_sudo} bundle exec foreman export upstart #{foreman_upstart_path} -a #{service_name} -u #{user} -l #{shared_path}/log #{concurrency}"
     end
 
     desc "Start the application services"
     task :start, roles: :app do
-      sudo "service foreman-#{application} start"
+      sudo "service #{service_name} start"
     end
 
     desc "Stop the application services"
     task :stop, roles: :app do
-      sudo "service foreman-#{application} stop"
+      sudo "service #{service_name} stop"
     end
 
     desc "Restart the application services"
     task :restart, roles: :app do
-      run "sudo service foreman-#{application} start || sudo service foreman-#{application} restart"
+      run "sudo service #{service_name} start || sudo service #{service_name}  restart"
     end
+  end
+
+  def service_name
+    "#{foreman_upstart_prefix}#{application}"
   end
 
   def concurrency
